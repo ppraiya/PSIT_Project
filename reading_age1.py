c832a2e86fb81c989187f7ffc255bf708ea5dc80
CCS\CCS_LP <59070105@it.kmitl.ac.th>
2016-11-21 17:30:42 +0700

    กราฟแบ่งตามช่วงอายุ

--- reading_age1.py
+++ reading_age1.py
@@ -7,11 +7,11 @@ def function():
 	you can test it in browser"""
 	import pygal                                                
 	pie_chart = Pie()
-	pie_chart.title = 'จ ํ านวนประชากรอายุ ต ั ้ งแต่  6ปี ข ึ ้ นไปจ ํ าแนกตามการอ่ านกลุ ่ มอายุ ปี พ.ศ.2558' #Table15
-	pie_chart.add('6-14',6694654 )
-	pie_chart.add('15-24',8581570)
-	pie_chart.add('25-39',12571923)
-	pie_chart.add('40-59',15077111)
-	pie_chart.add('60+',5483310)
+	pie_chart.title = 'จำนวนประชากรที่อ่านหนังสือ แบ่งตามช่วงอายุ ปีพ.ศ.2558' #Table15
+	pie_chart.add('อายุ 6-14',6694654 )
+	pie_chart.add('อายุ 15-24',8581570)
+	pie_chart.add('อายุ 25-39',12571923)
+	pie_chart.add('อายุ 40-59',15077111)
+	pie_chart.add('อายุ 60+',5483310)
 	pie_chart.render_to_file('age_read.svg')
 function()
